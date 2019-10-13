7815ed36606e4b5e445cbef9de36bd2701f0cdba
René Thomas Lottes <rene.lottes@gmx.de>
2019-10-13 15:56:03 +0200

    Datastruct for map

--- tests/mesh-generation.gd
+++ tests/mesh-generation.gd
@@ -2,16 +2,24 @@ extends Spatial
 
 onready var meshInstance = $MeshInstance;
 
+# [visbile, type]
+
 var terrain = [
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
-	[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-	[0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-	[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-	[0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+	[[0, 0], [1, 1], [1, 1], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [1, 1], [1, 1], [1, 1], [0, 0]],
+	[[0, 0], [0, 0], [1, 1], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0]],
+	[[0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0]],
+	[[0, 0], [1, 1], [1, 1], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0], [0, 0], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+	[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
 ]
 
 func _ready():
@@ -25,9 +33,9 @@ func _generate_mesh():
 	for y in range(terrain.size()):
 		for x in range(terrain[y].size()):
 			print(x, " / ", y, " -> ",terrain[y][x]);
-			if terrain[y][x] == 0:
+			if terrain[y][x][1] == 0:
 				_plane(st, x, y);
-			elif terrain[y][x] == 1:
+			elif terrain[y][x][1] >= 1:
 				_plane(st, x, y, -2);
 				_passage(st, x, y);
 	
@@ -45,14 +53,16 @@ func _generate_mesh():
 	
 	print(ResourceSaver.save("res://tests/testMesh.tres", mesh, 32));
 
-func _is_visible(x, y) -> bool:
+func _is_visible(x, y, checkPassage = true) -> bool:
 	if y > terrain.size() - 1:
 		return false;
 	elif x > terrain[y].size() - 1:
 		return false;
 	elif y <= 0 || x <= 0:
 		return false;
-	elif terrain[y][x] == 1:
+	elif !checkPassage && terrain[y][x][0] == 1:
+		return true;
+	elif checkPassage && (terrain[y][x][0] == 1 || terrain[y][x][1] >= 1):
 		return true;
 	return false;
 
@@ -62,25 +72,25 @@ func _is_visible(x, y) -> bool:
 # 2   3
 #
 func _get_color(corner: int, xOffset = 0, yOffset = 0) -> Color:
-	if _is_visible(xOffset, yOffset):
+	if _is_visible(xOffset, yOffset, false):
 		return Color(1, 0, 0);
 	match(corner):
 		0: 
-			if not _is_visible(xOffset - 1, yOffset + 1) && not _is_visible(xOffset, yOffset + 1) && not _is_visible(xOffset - 1, yOffset):
+			if not _is_visible(xOffset - 1, yOffset + 1, false) && not _is_visible(xOffset, yOffset + 1, false) && not _is_visible(xOffset - 1, yOffset, false):
 				return Color(0, 0, 0);
 		1: 
-			if not _is_visible(xOffset + 1, yOffset + 1) && not _is_visible(xOffset, yOffset + 1) && not _is_visible(xOffset + 1, yOffset):
+			if not _is_visible(xOffset + 1, yOffset + 1, false) && not _is_visible(xOffset, yOffset + 1, false) && not _is_visible(xOffset + 1, yOffset, false):
 				return Color(0, 0, 0);
 		2:
-			if not _is_visible(xOffset - 1, yOffset - 1) && not _is_visible(xOffset, yOffset - 1) && not _is_visible(xOffset - 1, yOffset):
+			if not _is_visible(xOffset - 1, yOffset - 1, false) && not _is_visible(xOffset, yOffset - 1, false) && not _is_visible(xOffset - 1, yOffset, false):
 				return Color(0, 0, 0);
 		3:
-			if not _is_visible(xOffset + 1, yOffset - 1) && not _is_visible(xOffset, yOffset - 1) && not _is_visible(xOffset + 1, yOffset):
+			if not _is_visible(xOffset + 1, yOffset - 1, false) && not _is_visible(xOffset, yOffset - 1, false) && not _is_visible(xOffset + 1, yOffset, false):
 				return Color(0, 0, 0);
 	return Color(1, 0, 0);
 
 func _plane(st: SurfaceTool, xOffset = 0, yOffset = 0, z = 0):
-	if not _is_visible(xOffset, yOffset) && (_is_visible(xOffset + 1, yOffset + 1) || _is_visible(xOffset - 1, yOffset - 1)):
+	if not _is_visible(xOffset, yOffset, false) && (_is_visible(xOffset + 1, yOffset + 1, false) || _is_visible(xOffset - 1, yOffset - 1, false)):
 		st.add_color(_get_color(0, xOffset, yOffset));
 		st.add_uv(Vector2(0, 1))
 		st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, z))
@@ -141,59 +151,77 @@ func _passage(st: SurfaceTool, xOffset = 0, yOffset = 0):
 		_passage_ceiling(st, xOffset, yOffset);
 
 func _passage_bottom(st: SurfaceTool, xOffset = 0, yOffset = 0):
+	st.add_color(_get_color(2, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0))
 	
+	st.add_color(_get_color(2, xOffset, yOffset));
 	st.add_uv(Vector2(0, 0))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(3, xOffset, yOffset));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(3, xOffset, yOffset));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(3, xOffset, yOffset));
 	st.add_uv(Vector2(1, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, 0))
 	
+	st.add_color(_get_color(3, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0))
 
 func _passage_right(st: SurfaceTool, xOffset = 0, yOffset = 0):
+	st.add_color(_get_color(3, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, 0))
 	
+	st.add_color(_get_color(3, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(1, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(1, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(1, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, 0))
 	
+	st.add_color(_get_color(3, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, 0))
 
 func _passage_left(st: SurfaceTool, xOffset = 0, yOffset = 0):
+	st.add_color(_get_color(2, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0))
 	
+	st.add_color(_get_color(0, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(2, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, -2))
 	
+	st.add_color(_get_color(2, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0))
 	
+	st.add_color(_get_color(0, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, 0))
 	
+	st.add_color(_get_color(0, xOffset, yOffset));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, -2))
 
