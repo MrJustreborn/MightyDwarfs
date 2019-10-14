3f28e3fb88763489556dd584a16b2f0f2953e8da
René Thomas Lottes <rene.lottes@gmx.de>
2019-10-14 16:47:07 +0200

    Chuck terrain

--- tests/mesh-generation.gd
+++ tests/mesh-generation.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-onready var meshInstance = $MeshInstance;
+onready var chunks = $Chunks;
 
 # [visbile, depth]
 
@@ -27,25 +27,45 @@ var terrain = [
 	[[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
 ]
 
+const CHUNK_SIZE = 10;
+const CUBE_SIZE = 2;
 func _ready():
-	_generate_mesh();
+	var yS = ceil(terrain.size() / float(CHUNK_SIZE));
+	var xS = ceil(terrain[0].size() / float(CHUNK_SIZE));
+	
+	print("Size: ", xS, " - ", yS)
+	
+	for y in range(yS):
+		for x in range(xS):
+			print(x, " - ",y)
+			var mesh = _generate_mesh(x, y);
+			var mI = MeshInstance.new();
+			mI.mesh = mesh;
+			mI.material_override = load("res://tests/new_Spatialmaterial.tres");
+			mI.translate(Vector3(x * CHUNK_SIZE * CUBE_SIZE, y * CHUNK_SIZE * CUBE_SIZE, 0));
+			chunks.add_child(mI);
+			mI.create_trimesh_collision();
+			#TODO: check dynamic memory leaks
 
 
-func _generate_mesh():
+func _generate_mesh(xOff = 0, yOff = 0):
 	var st = SurfaceTool.new();
 	st.begin(Mesh.PRIMITIVE_TRIANGLES);
 	
-	for y in range(terrain.size()):
-		for x in range(terrain[y].size()):
-			#print(x, " / ", y, " -> ",terrain[y][x]);
+	for y in range(CHUNK_SIZE):#range(terrain.size()):
+		for x in range(CHUNK_SIZE):#range(terrain[y].size()):
+			var yWorld = y + yOff * CHUNK_SIZE;
+			var xWorld = x + xOff * CHUNK_SIZE;
+			if yWorld >= terrain.size() || xWorld >= terrain[yWorld].size():
+				continue
 			#if terrain[y][x][0] == 0: # Fog of War
 			#	_plane(st, x, y);
-			if terrain[y][x][1] == 0:
-				_plane(st, x, y);
-			elif terrain[y][x][1] >= 1:
-				_plane(st, x, y, -2 * terrain[y][x][1]);
-				for i in range(terrain[y][x][1]):
-					_passage(st, x, y, -2 * i);
+			if terrain[yWorld][xWorld][1] == 0:
+				_plane(st, xWorld, yWorld, 0, x, y);
+			elif terrain[yWorld][xWorld][1] >= 1:
+				_plane(st, xWorld, yWorld, -2 * terrain[yWorld][xWorld][1], x, y);
+				for i in range(terrain[yWorld][xWorld][1]):
+					_passage(st, xWorld, yWorld, -2 * i, x, y);
 	
 	st.index();
 	st.generate_normals();
@@ -53,13 +73,9 @@ func _generate_mesh():
 	
 	# Commit to a mesh.
 	var mesh = st.commit()
+	return mesh;
 	
-	meshInstance.mesh = mesh;
-	meshInstance.create_trimesh_collision();
-	
-	meshInstance.material_override = load("res://tests/new_Spatialmaterial.tres");
-	
-	print(ResourceSaver.save("res://tests/testMesh.tres", mesh, 32));
+	#print(ResourceSaver.save("res://tests/testMesh.tres", mesh, 32));
 
 func _is_visible(x, y, checkPassage = true) -> bool:
 	if y > terrain.size() - 1:
@@ -110,163 +126,163 @@ func _get_color(corner: int, xOffset = 0, yOffset = 0) -> Color:
 				return Color(0, 0, 0);
 	return Color(1, 1, 1);
 
-func _plane(st: SurfaceTool, xOffset = 0, yOffset = 0, z = 0):
-	if not _is_visible(xOffset, yOffset, false) && (_is_visible(xOffset + 1, yOffset + 1, false) || _is_visible(xOffset - 1, yOffset - 1, false)):
-		st.add_color(_get_color(0, xOffset, yOffset));
+func _plane(st: SurfaceTool, x = 0, y = 0, z = 0, xOffset = 0, yOffset = 0):
+	if not _is_visible(x, y, false) && (_is_visible(x + 1, y + 1, false) || _is_visible(x - 1, y - 1, false)):
+		st.add_color(_get_color(0, x, y));
 		st.add_uv(Vector2(0, 1))
 		st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, z))
 		
-		st.add_color(_get_color(1, xOffset, yOffset));
+		st.add_color(_get_color(1, x, y));
 		st.add_uv(Vector2(1, 1))
 		st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, z))
 		
-		st.add_color(_get_color(3, xOffset, yOffset));
+		st.add_color(_get_color(3, x, y));
 		st.add_uv(Vector2(1, 0))
 		st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, z))
 		
-		st.add_color(_get_color(3, xOffset, yOffset));
+		st.add_color(_get_color(3, x, y));
 		st.add_uv(Vector2(1, 0))
 		st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, z))
 		
-		st.add_color(_get_color(2, xOffset, yOffset));
+		st.add_color(_get_color(2, x, y));
 		st.add_uv(Vector2(0, 0))
 		st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, z))
 		
-		st.add_color(_get_color(0, xOffset, yOffset));
+		st.add_color(_get_color(0, x, y));
 		st.add_uv(Vector2(0, 1))
 		st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, z))
 	else:
-		st.add_color(_get_color(2, xOffset, yOffset));
+		st.add_color(_get_color(2, x, y));
 		st.add_uv(Vector2(0, 0))
 		st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, z))
 		
-		st.add_color(_get_color(0, xOffset, yOffset));
+		st.add_color(_get_color(0, x, y));
 		st.add_uv(Vector2(0, 1))
 		st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, z))
 		
-		st.add_color(_get_color(1, xOffset, yOffset));
+		st.add_color(_get_color(1, x, y));
 		st.add_uv(Vector2(1, 1))
 		st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, z))
 		
-		st.add_color(_get_color(1, xOffset, yOffset));
+		st.add_color(_get_color(1, x, y));
 		st.add_uv(Vector2(1, 1))
 		st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, z))
 		
-		st.add_color(_get_color(3, xOffset, yOffset));
+		st.add_color(_get_color(3, x, y));
 		st.add_uv(Vector2(1, 0))
 		st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, z))
 		
-		st.add_color(_get_color(2, xOffset, yOffset));
+		st.add_color(_get_color(2, x, y));
 		st.add_uv(Vector2(0, 0))
 		st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, z))
 
-func _passage(st: SurfaceTool, xOffset = 0, yOffset = 0, z = 0):
-	if _is_wall(xOffset, yOffset - 1, -z/2):
-		_passage_bottom(st, xOffset, yOffset, z);
-	if _is_wall(xOffset + 1, yOffset, -z/2):
-		_passage_right(st, xOffset, yOffset, z);
-	if _is_wall(xOffset - 1, yOffset, -z/2):
-		_passage_left(st, xOffset, yOffset, z);
-	if _is_wall(xOffset, yOffset + 1, -z/2):
-		_passage_ceiling(st, xOffset, yOffset, z);
+func _passage(st: SurfaceTool, x = 0, y = 0, z = 0, xOffset = 0, yOffset = 0):
+	if _is_wall(x, y - 1, -z/2):
+		_passage_bottom(st, x, y, z, xOffset, yOffset);
+	if _is_wall(x + 1, y, -z/2):
+		_passage_right(st, x, y, z, xOffset, yOffset);
+	if _is_wall(x - 1, y, -z/2):
+		_passage_left(st, x, y, z, xOffset, yOffset);
+	if _is_wall(x, y + 1, -z/2):
+		_passage_ceiling(st, x, y, z, xOffset, yOffset);
 
-func _passage_bottom(st: SurfaceTool, xOffset = 0, yOffset = 0, z = 0):
-	st.add_color(_get_color(2, xOffset, yOffset));
+func _passage_bottom(st: SurfaceTool, x = 0, y = 0, z = 0, xOffset = 0, yOffset = 0):
+	st.add_color(_get_color(2, x, y));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(2, xOffset, yOffset));
+	st.add_color(_get_color(2, x, y));
 	st.add_uv(Vector2(0, 0))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(3, xOffset, yOffset));
+	st.add_color(_get_color(3, x, y));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(3, xOffset, yOffset));
+	st.add_color(_get_color(3, x, y));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(3, xOffset, yOffset));
+	st.add_color(_get_color(3, x, y));
 	st.add_uv(Vector2(1, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(2, xOffset, yOffset));
+	st.add_color(_get_color(2, x, y));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0 + z))
 
-func _passage_right(st: SurfaceTool, xOffset = 0, yOffset = 0, z = 0):
-	st.add_color(_get_color(3, xOffset, yOffset));
+func _passage_right(st: SurfaceTool, x = 0, y = 0, z = 0, xOffset = 0, yOffset = 0):
+	st.add_color(_get_color(3, x, y));
 	st.add_uv(Vector2(0, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(3, xOffset, yOffset));
+	st.add_color(_get_color(3, x, y));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(1, xOffset, yOffset));
+	st.add_color(_get_color(1, x, y));
 	st.add_uv(Vector2(1, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(1, xOffset, yOffset));
+	st.add_color(_get_color(1, x, y));
 	st.add_uv(Vector2(1, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(1, xOffset, yOffset));
+	st.add_color(_get_color(1, x, y));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(3, xOffset, yOffset));
+	st.add_color(_get_color(3, x, y));
 	st.add_uv(Vector2(0, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, -1 + yOffset*2, 0 + z))
 
-func _passage_left(st: SurfaceTool, xOffset = 0, yOffset = 0, z = 0):
-	st.add_color(_get_color(2, xOffset, yOffset));
+func _passage_left(st: SurfaceTool, x = 0, y = 0, z = 0, xOffset = 0, yOffset = 0):
+	st.add_color(_get_color(2, x, y));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(0, xOffset, yOffset));
+	st.add_color(_get_color(0, x, y));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(2, xOffset, yOffset));
+	st.add_color(_get_color(2, x, y));
 	st.add_uv(Vector2(0, 0))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(2, xOffset, yOffset));
+	st.add_color(_get_color(2, x, y));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(-1 + xOffset*2, -1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(0, xOffset, yOffset));
+	st.add_color(_get_color(0, x, y));
 	st.add_uv(Vector2(1, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(0, xOffset, yOffset));
+	st.add_color(_get_color(0, x, y));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, -2 + z))
 
-func _passage_ceiling(st: SurfaceTool, xOffset = 0, yOffset = 0, z = 0):
-	st.add_color(_get_color(1, xOffset, yOffset));
+func _passage_ceiling(st: SurfaceTool, x = 0, y = 0, z = 0, xOffset = 0, yOffset = 0):
+	st.add_color(_get_color(1, x, y));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(1, xOffset, yOffset));
+	st.add_color(_get_color(1, x, y));
 	st.add_uv(Vector2(1, 1))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(0, xOffset, yOffset));
+	st.add_color(_get_color(0, x, y));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(0, xOffset, yOffset));
+	st.add_color(_get_color(0, x, y));
 	st.add_uv(Vector2(0, 1))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, -2 + z))
 	
-	st.add_color(_get_color(0, xOffset, yOffset));
+	st.add_color(_get_color(0, x, y));
 	st.add_uv(Vector2(0, 0))
 	st.add_vertex(Vector3(-1 + xOffset*2, 1 + yOffset*2, 0 + z))
 	
-	st.add_color(_get_color(1, xOffset, yOffset));
+	st.add_color(_get_color(1, x, y));
 	st.add_uv(Vector2(1, 0))
 	st.add_vertex(Vector3(1 + xOffset*2, 1 + yOffset*2, 0 + z))
 
