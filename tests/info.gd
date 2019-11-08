4967837a2d7c819cfc2e69ddfe2ed5a5239f30dd
René Thomas Lottes <rene.lottes@gmx.de>
2019-11-08 12:42:09 +0100

    Test info mesh v2

--- tests/info.gd
+++ tests/info.gd
@@ -16,9 +16,11 @@ func set_mouse_pos(pos: Vector2):
 	if x % 2 == 0 && y % 2 == 0 && !start_pos:
 		$MeshInstance.translation.x = pos.x * 2;
 		$MeshInstance.translation.y = pos.y * 2;
+		$MeshInstance.get_surface_material(0).set_shader_param("blue_tint", get_color(x, y))
 	elif x % 2 == 0 && y % 2 == 0 && (start_pos.x == x || start_pos.y == y):
 		$MeshInstance.translation.x = pos.x * 2;
 		$MeshInstance.translation.y = pos.y * 2;
+		$MeshInstance.get_surface_material(0).set_shader_param("blue_tint", get_color(x, y))
 		if end_pos != pos:
 			end_pos = pos;
 			generate_mesh()
@@ -30,6 +32,11 @@ func set_mouse_pos(pos: Vector2):
 		#print("COMPLETE:", start_pos, end_pos)
 		start_pos = null;
 		end_pos = null;
+
+func get_color(x, y):
+	if get_parent()._is_wall(x, y):
+		return Color(0, 1, 0)
+	return Color(1, 0, 0)
 
 func generate_mesh():
 	print("Update info mesh")
