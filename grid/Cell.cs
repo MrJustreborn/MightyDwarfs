d2df775c280d7a3280b435715a5792ca369a6e26
René Thomas Lottes <rene.lottes@gmx.de>
2020-01-12 08:31:35 +0100

    Added properties to Cell class

--- grid/Cell.cs
+++ grid/Cell.cs
@@ -3,7 +3,15 @@ using Godot;
 
 namespace Grid
 {
+    public enum Visible {
+        dark = 0,
+        visible = 1,
+        fog = 2
+    }
     public class Cell : Reference {
-        
+        public Visible visible {get; private set;} = Visible.dark;
+        public int depth {get; private set;} = 0;
+        public int type {get; private set;} = 0;
+        public int damage {get; private set;} = 0;
     }
 }
