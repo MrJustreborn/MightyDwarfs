79325f7cc875fb4f1c4da05e2710b8dad23fd8ca
René Thomas Lottes <rene.lottes@gmx.de>
2020-05-03 10:54:35 +0200

    Update fog

--- grid/Cell.cs
+++ grid/Cell.cs
@@ -13,9 +13,13 @@ namespace Grid
 
         private const int MAX_DAMAGE = 5;
 
+        private readonly int x;
+        private readonly int y;
+        private readonly int chunk_size;
+
         public Visible visible {get; private set;} = Visible.dark;
         public int depth {get; private set;} = 0;
-        public int type {get; private set;} = 0;
+        public int type {get; private set;} = 0; //TODO create type-db
         public int damage {get; private set;} = 0;
 
 
@@ -23,6 +27,12 @@ namespace Grid
         private Visible visibleInNextFrame = Visible.dark;
         private int depthInNextFrame = 0;
         private int damageInNextFrame = 0;
+
+        public Cell(int x, int y, int chunk_size) {
+            self.x = x;
+            self.y = y;
+            self.chunk_size = chunk_size;
+        }
 
         //TODO: track state for current and next frame
         public void SetToFogIfVisibleInNextFrame() {
@@ -35,8 +45,14 @@ namespace Grid
             visibleInNextFrame = Visible.visible;
         }
 
+        public void SetToVisibleIfFogInNextFrame() {
+            if (visible = Visible.fog) {
+                visibleInNextFrame = Visible.visible;
+            }
+        }
+
         public void TakeDamgeInNextFrame(int amt) {
-            damageInNextFrame += amt;
+            damageInNextFrame = damage + amt; //Prevent multi damage per frame
 
             if (damageInNextFrame >= MAX_DAMAGE) {
                 depthInNextFrame += 1;
--- grid/Grid.cs
+++ grid/Grid.cs
@@ -3,7 +3,10 @@ using Godot;
 
 namespace Grid
 {
-    public class Grid : Reference {
+    public class Grid : Node {
+
+        private const int CHUNK_SIZE = 5;
+
         private List<List<Cell>> pp = new List<List<Cell>>(); //upper-right
         private List<List<Cell>> pn = new List<List<Cell>>(); //lower-right
         private List<List<Cell>> nn = new List<List<Cell>>(); //lower-left
@@ -43,6 +46,73 @@ namespace Grid
             return null; //This does not exists so return null ... ?
         }
 
+        public void _Process(float delta) {
+            firstUpdateCallFlag = true;
+        }
+
+        //TODO: create list of chunks to regenerate mesh (fog/new depth etc)
+        private bool regenerateMesh = false;
+        private bool firstUpdateCallFlag = true; //TODO reset each frame (_Process)
+        public void update(int x, int y, int radius_remove_fog = 5, int radius_discover_hidden = 2) {
+            //Create FogOfWar
+            if (firstUpdateCallFlag) {
+                firstUpdateCallFlag = false;
+                resetAllToFog(pp);
+                resetAllToFog(pn);
+                resetAllToFog(nn);
+                resetAllToFog(np);
+            }
+
+            markVisible(x, y, radius_remove_fog, true);
+            markVisible(x, y, radius_discover_hidden, false);
+        }
+
+        private void resetAllToFog(List<List<Cell>> which) {
+            for (int x = 0; x < which.Count; x++) {
+                for (int y = 0; y < which[x].Count; y++) {
+                    which[x][y]?.SetToFogIfVisibleInNextFrame();
+                }
+            }
+        }
+
+        //Use circle isntead of rect, it's radius not size^^
+        private void markVisible(int x, int y, int radius, bool fogOnly = true) {
+            for (int xOff = 0; xOff < radius; xOff++) {
+                if (fogOnly) {
+                    GetCell(x - xOff, y)?.SetToVisibleIfFogInNextFrame();
+                    GetCell(x + xOff, y)?.SetToVisibleIfFogInNextFrame();
+                } else {
+                    GetCell(x - xOff, y)?.SetToVisibleInNextFrame();
+                    GetCell(x + xOff, y)?.SetToVisibleInNextFrame();
+                }
+            }
+            for (int yOff = 0; yOff < radius; yOff++) {
+                if (fogOnly) {
+                    GetCell(x, y - yOff)?.SetToVisibleIfFogInNextFrame();
+                    GetCell(x, y + yOff)?.SetToVisibleIfFogInNextFrame();
+                } else {
+                    GetCell(x, y - yOff)?.SetToVisibleInNextFrame();
+                    GetCell(x, y + yOff)?.SetToVisibleInNextFrame();
+                }
+            }
+        }
+
+        //TODO: return chunks for regeneration each frame
+        public List<List<Cell>> getChangedChunks() {
+            for (int x = 0; x < pp.Count; x++) {
+                for (int y = 0; y < pp[x].Count; y++) {
+                    if(pp[x][y] != null && pp[x][y].UpdateInNextFrame(false)) {
+                        List<Cell> chunk = getAllCellsInChunk(pp[x][y]);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private List<Cell> getAllCellsInChunk(Cell which) {
+            return null;
+        }
+
         private void _SetCell(List<List<Cell>> which, int x, int y, Cell what) {
             x = Mathf.Abs(x);
             y = Mathf.Abs(y);
@@ -53,7 +123,7 @@ namespace Grid
             }
 
             while (which[x].Count <= y) {
-                which[x].Add(new Cell());
+                which[x].Add(new Cell(x, y, CHUNK_SIZE));
             }
 
             GD.Print(GD.Str(which.Count), " - ", GD.Str(which[x].Count), " : ", GD.Str(x), " - ", GD.Str(y));
